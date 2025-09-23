9da46da7706dde68bda42bfd5b86613e8947937a
Thales Silva <thales.ferreira@extreme.digital>
2025-09-23 13:40:22 -0300

    adicionando front end com angular

--- migrations/check_livros_structure.sql
+++
@@ -1,5 +0,0 @@
--- Verificar estrutura exata da tabela livros
-SELECT column_name, data_type, is_nullable
-FROM information_schema.columns 
-WHERE table_schema = 'biblioteca' AND table_name = 'livros'
-ORDER BY ordinal_position;
